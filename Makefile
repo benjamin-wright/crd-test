031a3c77578ec661ad8b451b70a54d7bbee25b40
Ben Wright <benjamin.p.wright@hotmail.com>
2020-01-31 22:19:36 +0000

    rust image now builds

--- Makefile
+++ Makefile
@@ -1,13 +1,20 @@
-.PHONY: start stop start-k8s install clean
+.PHONY: build start stop start-k8s install clean images
 
 default:
 	echo "  ----  CRD-TEST  ----"
 	echo ""
+	echo " images    - build dev images"
 	echo " start     - spin up everything from a standing start"
 	echo " stop      - tear down EVERYTHING"
 	echo " start-k8s - create a new k8s cluster"
 	echo " install   - install the helm chart"
 	echo " clean     - tear down the helm chart"
+
+build:
+	cd minion-controller && make build
+
+images:
+	docker build -t rust ./images/dev
 
 start: start-k8s install
 
--- minion-controller/src/main.rs
+++ minion-controller/src/main.rs
@@ -1,5 +1,11 @@
 #[macro_use]
-extern create serde_derive;
+extern crate serde_derive;
+
+use kube::{
+    api::{Object, RawApi, Informer, WatchEvent, Void},
+    client::APIClient,
+    config,
+};
 
 #[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct Task {
@@ -8,6 +14,27 @@ pub struct Task {
     pub inputs: Option<Vec<String>>,
 }
 
+type KubeTask = Object<Task, Void>;
+
 fn main() {
-    println!("Hello, world!");
+    let kubeconfig = config::load_kube_config().expect("kubeconfig failed to load");
+    let client = APIClient::new(kubeconfig);
+    let namespace = "default";
+    let resource = RawApi::customResource("tasks")
+        .group("minion.ponglehub.com")
+        .within(&namespace);
+
+    let informer = Informer::raw(client, resource).init().expect("informer init failed");
+    loop {
+        informer.poll().expect("informer poll failed");
+
+        // Now we just do something each time a new book event is triggered.
+        while let Some(event) = informer.pop() {
+            handle(event);
+        }
+    }
+}
+
+fn handle(event: WatchEvent<KubeTask>) {
+    println!("Something happened to a task")
 }
