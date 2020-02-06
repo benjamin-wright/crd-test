9f54f1001273488446e904ee32749525a258e2f1
Ben Wright <benjamin.wright@nielsen.com>
2020-02-06 13:40:36 +0000

    listing tasks properly

--- minion-controller/src/main.rs
+++ minion-controller/src/main.rs
@@ -3,7 +3,7 @@ extern crate serde_derive;
 
 use futures::StreamExt;
 use kube::{
-    api::{Informer, ListParams, Object, RawApi, Void, WatchEvent},
+    api::{Informer, Object, RawApi, Void, WatchEvent, Reflector},
     client::APIClient,
     config,
 };
@@ -19,29 +19,66 @@ type KubeTask = Object<Task, Void>;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+    prepare_state().await?;
+    listen_for_changes().await?;
+
+    Ok(())
+}
+
+fn get_tasks() -> RawApi {
+    return RawApi::customResource("tasks")
+        .group("minion.ponglehub.com");
+}
+
+async fn prepare_state() -> Result<(), anyhow::Error> {
     // Load the kubeconfig file.
     let kubeconfig = config::incluster_config().expect("Failed to load kube config");
 
     // Create a new client
     let client = APIClient::new(kubeconfig);
 
-    let tasks = RawApi::customResource("tasks")
-        .group("minion.ponglehub.com");
+    let tasks_api = get_tasks();
 
-    for t in tasks.list(&ListParams::default()).unwrap() {
-        loadTask(t);
-    }
+    let task_reflector: Reflector<KubeTask> = Reflector::raw(client, tasks_api)
+        .timeout(10)
+        .init()
+        .await?;
+
+    task_reflector.poll().await?;
+
+    task_reflector.state().await.into_iter().for_each(|tasks| {
+        for task in &tasks {
+            println!(
+                "Found Task {}:{} from pipeline '{}'",
+                task.metadata.namespace.as_ref().expect("Namespace not defined"),
+                task.metadata.name,
+                task.spec.pipeline
+            );
+        }
+    });
+
+    return Ok(());
+}
+
+async fn listen_for_changes() -> anyhow::Result<()> {
+    // Load the kubeconfig file.
+    let kubeconfig = config::incluster_config().expect("Failed to load kube config");
+
+    // Create a new client
+    let client = APIClient::new(kubeconfig);
+
+    let tasks_api = get_tasks();
 
     // Create our informer and start listening.
-    let informer = Informer::raw(client, tasks)
+    let informer = Informer::raw(client, tasks_api)
         .init()
         .await?;
 
     loop {
-        let mut taskEvents = informer.poll().await?.boxed();
+        let mut task_events = informer.poll().await?.boxed();
 
         // Now we just do something each time a new task event is triggered.
-        while let Some(event) = taskEvents.next().await {
+        while let Some(event) = task_events.next().await {
             handle(event?);
         }
     }
@@ -49,21 +86,21 @@ async fn main() -> anyhow::Result<()> {
 
 fn handle(event: WatchEvent<KubeTask>) {
     match event {
-        WatchEvent::Added(task) => loadTask(task),
-        WatchEvent::Deleted(task) => removedTask(task),
+        WatchEvent::Added(task) => load_task(task),
+        WatchEvent::Deleted(task) => removed_task(task),
         _ => println!("another event"),
     }
 }
 
-fn loadTask(task: KubeTask) {
+fn load_task(task: KubeTask) {
     println!(
         "Added a task {}:{} from pipeline '{}'",
-        task.metadata.namespace.expect("Namespace not defined"),
+        task.metadata.namespace.as_ref().expect("Namespace not defined"),
         task.metadata.name,
         task.spec.pipeline
-    )
+    );
 }
 
-fn removedTask(task: KubeTask) {
-    println!("Deleted a task {}", task.metadata.name)
+fn removed_task(task: KubeTask) {
+    println!("Deleted a task {}", task.metadata.name);
 }
