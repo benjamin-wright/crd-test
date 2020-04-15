38f2da9505a5c5f7002bc5051c017dddb060a9e4
Ben Wright <benjamin.wright@nielsen.com>
2020-04-15 08:03:08 +0100

    moving .devspace temp file removal to cleanup script, and updating it to be more dynamic

--- scripts/start.sh
+++ scripts/start.sh
@@ -38,8 +38,4 @@ kubectl create namespace infra
 helm dep update infrastructure/helm
 helm upgrade -i --wait infra --namespace infra infrastructure/helm
 
-echo "cleaning out old files"
-rm -r .devspace
-rm -r components/operators/pipeline-monitor/.devspace
-
 devspace run install-crds
--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
 kind delete cluster --name $CLUSTER_NAME
+
+files=$(find . -name .devspace)
+for file in $files; do
+    echo "removing temp dir: $file"
+    rm -r $file;
+done
